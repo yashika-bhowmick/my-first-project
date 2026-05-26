1aeb1aaa5f49f1c148b16da31ef6c26c0d780a7c
Yashika <yashikabhowmick25@navgurukul.org>
2026-05-26 11:55:18 +0530

    Ready for deployment to heroku.

---
+++ learning_logs/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from learning_logs.models import Topic, Entry
+
+# Register your models here.
+from learning_logs.models import Topic
+admin.site.register(Topic)
+admin.site.register(Entry)
+
+
